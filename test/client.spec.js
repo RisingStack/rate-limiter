08d173a34d44730061292229982e4043692d275d
Dávid Szakállas <david.szakallas@gmail.com>
2017-05-02 14:38:56 +0200

    fix: key append check no longer filters zeros

--- test/client.spec.js
+++ test/client.spec.js
@@ -1,33 +1,26 @@
-'use strict'
+'use strict';
 
-var expect = require('chai').expect
+var expect = require('chai').expect;
 var sinon = require('sinon');
 var microtime = require('@risingstack/microtime');
-var version = require('../lib/redis/version');
-var rateLimiter = require('../lib/client')
+var rateLimiter = require('../lib/client');
+var Script = require('../lib/script');
 
 describe('rateLimiter', function () {
 
   describe('options', function () {
+    var options;
 
     describe('single', function () {
-      var options;
-
       beforeEach(function () {
         options = {
-          redis: {defineCommand: sinon.stub()},
+          redis: { },
           interval: 10000,
           maxInInterval: 5,
           minDifference: 500,
           buckets: 10000,
           namespace: 'MyNamespace'
         };
-        options.redis['rateLimiter' + version] = sinon.stub();
-      });
-
-      it('throws if redis doesn\'t expose `defineCommand`', function () {
-        options.redis = {};
-        expect(rateLimiter.bind(null, options)).to.throw();
       });
 
       it('throws if interval is missing', function () {
@@ -62,78 +55,81 @@ describe('rateLimiter', function () {
 
       describe('limiter', function () {
         var limiter;
+        var evalScript;
         beforeEach(function (done) {
           rateLimiter(options, function (err, lim) {
             limiter = lim;
             done(err);
           });
           sinon.stub(microtime, 'now').returns(3000);
+          evalScript = sinon.stub(Script.prototype, 'eval');
         });
         afterEach(function () {
           microtime.now.restore();
+          evalScript.restore();
         });
         it('throws an error when called without callback', function () {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           expect(limiter.bind(null)).to.throw();
         });
         it('throws an error when called with invalid increment', function () {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           expect(limiter.bind(null, -3, function () { })).to.throw();
         });
-        it('calls redis.defineCommand with expected arguments when called without id and increment', function (done) {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+        it('calls script.eval with expected arguments when called without id and increment', function (done) {
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           limiter(function (err, data) {
             if (err) {
               return done(err);
             }
             try {
-              expect(options.redis['rateLimiter' + version].args[0][0]).to.eql(
+              expect(evalScript.args[0][1]).to.eql(
                 [1, 'MyNamespace', 1, 1, 3000, 500 * 1000, 5, 1000, 10000]);
               expect(data).eql({
                 actionsRemaining: 0,
                 actionsRecorded: 1,
                 wait: 2
-              })
+              });
               return done();
             } catch (err) {
               return done(err);
             }
           });
         });
-        it('calls redis.defineCommand with expected arguments when called with increment', function (done) {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+        it('calls script.eval with expected arguments when called with increment', function (done) {
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           limiter(3, function (err, data) {
             if (err) {
               return done(err);
             }
             try {
-              expect(options.redis['rateLimiter' + version].args[0][0]).to.eql(
+              expect(evalScript.args[0][1]).to.eql(
                 [1, 'MyNamespace', 1, 3, 3000, 500 * 1000, 5, 1000, 10000]);
               expect(data).eql({
                 actionsRemaining: 0,
                 actionsRecorded: 1,
                 wait: 2
-              })
+              });
               return done();
             } catch (err) {
               return done(err);
             }
           });
         });
-        it('calls redis.defineCommand with expected arguments when called with id and increment', function (done) {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+        it('calls script.eval with expected arguments when called with id and increment', function (done) {
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           limiter('action', 3, function (err, data) {
             if (err) {
               return done(err);
             }
             try {
-              expect(options.redis['rateLimiter' + version].args[0][0]).to.eql(
+              expect(evalScript.args[0][1]).to.eql(
                 [1, 'MyNamespace:action', 1, 3, 3000, 500 * 1000, 5, 1000, 10000]);
               expect(data).eql({
                 actionsRemaining: 0,
                 actionsRecorded: 1,
                 wait: 2
-              })
+              });
               return done();
             } catch (err) {
               return done(err);
@@ -141,18 +137,13 @@ describe('rateLimiter', function () {
           });
         });
       });
-    })
+    });
 
     describe('multi array', function () {
       var options;
-
       beforeEach(function () {
         options = {
-          redis: {
-            defineCommand: function () {
-              return 0;
-            }
-          },
+          redis: { },
           namespace: 'MyNamespace',
           limits: [{
             interval: 10000,
@@ -163,8 +154,6 @@ describe('rateLimiter', function () {
             maxInInterval: 25
           }]
         };
-        options.redis['rateLimiter' + version] = sinon.stub();
-
       });
 
       it('throws if array is empty', function () {
@@ -174,11 +163,6 @@ describe('rateLimiter', function () {
 
       it('throws if interval is missing', function () {
         delete options.limits[1].interval;
-        expect(rateLimiter.bind(null, options)).to.throw();
-      });
-
-      it('throws if redis doesn\'t expose `defineCommand`', function () {
-        options.redis = {};
         expect(rateLimiter.bind(null, options)).to.throw();
       });
 
@@ -209,70 +193,74 @@ describe('rateLimiter', function () {
 
       describe('limiter', function () {
         var limiter;
+        var evalScript;
         beforeEach(function (done) {
           rateLimiter(options, function (err, lim) {
             limiter = lim;
             done(err);
           });
           sinon.stub(microtime, 'now').returns(3000);
+          evalScript = sinon.stub(Script.prototype, 'eval');
         });
         afterEach(function () {
           microtime.now.restore();
+          evalScript.restore();
         });
-        it('calls redis.defineCommand with expected arguments when called without id and increment', function (done) {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+
+        it('calls script.eval with expected arguments when called without id and increment', function (done) {
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           limiter(function (err, data) {
             if (err) {
               return done(err);
             }
             try {
-              expect(options.redis['rateLimiter' + version].args[0][0]).to.eql(
-                [2, 'MyNamespace', 'MyNamespace:1', 1, 1, 3000, 500 * 1000, 0 * 1000, 5, 25, 1000, 10000, 10000, 10000]);
+              expect(evalScript.args[0][1]).to.eql(
+                [2, 'MyNamespace:0', 'MyNamespace:1', 1, 1, 3000, 500 * 1000, 0 * 1000, 5, 25, 1000, 10000, 10000, 10000]);
               expect(data).eql({
                 actionsRemaining: 0,
                 actionsRecorded: 1,
                 wait: 2
-              })
+              });
               return done();
             } catch (err) {
               return done(err);
             }
           });
         });
-        it('calls redis.defineCommand with expected arguments when called with increment', function (done) {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+        it('calls script.eval with expected arguments when called with increment', function (done) {
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           limiter(3, function (err, data) {
             if (err) {
               return done(err);
             }
             try {
-              expect(options.redis['rateLimiter' + version].args[0][0]).to.eql(
-                [2, 'MyNamespace', 'MyNamespace:1', 1, 3, 3000, 500 * 1000, 0 * 1000, 5, 25, 1000, 10000, 10000, 10000]);
+              expect(evalScript.args[0][1]).to.eql(
+                [2, 'MyNamespace:0', 'MyNamespace:1', 1, 3, 3000, 500 * 1000, 0 * 1000, 5, 25, 1000, 10000, 10000, 10000]);
               expect(data).eql({
                 actionsRemaining: 0,
                 actionsRecorded: 1,
                 wait: 2
-              })
+              });
               return done();
             } catch (err) {
               return done(err);
             }
           });
         });
-        it('calls redis.defineCommand with expected arguments when called with id and increment', function (done) {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+        it('calls script.eval with expected arguments when called with id and increment', function (done) {
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           limiter('action', 3, function (err, data) {
             if (err) {
               return done(err);
             }
             try {
-              expect(options.redis['rateLimiter' + version].args[0][0]).to.eql(
-                [2, 'MyNamespace:action', 'MyNamespace:1:action', 1, 3, 3000, 500 * 1000, 0 * 1000, 5, 25, 1000, 10000, 10000, 10000]);
+              expect(evalScript.args[0][1]).to.eql(
+                [2, 'MyNamespace:0:action', 'MyNamespace:1:action', 1, 3, 3000, 500 * 1000, 0 * 1000, 5, 25, 1000, 10000, 10000, 10000]);
               expect(data).eql({
                 actionsRemaining: 0,
                 actionsRecorded: 1,
                 wait: 2
-              })
+              });
               return done();
             } catch (err) {
               return done(err);
@@ -287,11 +275,7 @@ describe('rateLimiter', function () {
 
       beforeEach(function () {
         options = {
-          redis: {
-            defineCommand: function () {
-              return 0;
-            }
-          },
+          redis: { },
           namespace: 'MyNamespace',
           limits: {
             a: {
@@ -304,8 +288,7 @@ describe('rateLimiter', function () {
               maxInInterval: 25
             }
           }
-        }
-        options.redis['rateLimiter' + version] = sinon.stub();
+        };
       });
 
       it('throws if array is empty', function () {
@@ -315,11 +298,6 @@ describe('rateLimiter', function () {
 
       it('throws if interval is missing', function () {
         delete options.limits.b.interval;
-        expect(rateLimiter.bind(null, options)).to.throw();
-      });
-
-      it('throws if redis doesn\'t expose `defineCommand`', function () {
-        options.redis = {};
         expect(rateLimiter.bind(null, options)).to.throw();
       });
 
@@ -350,70 +328,73 @@ describe('rateLimiter', function () {
 
       describe('limiter', function () {
         var limiter;
+        var evalScript;
         beforeEach(function (done) {
           rateLimiter(options, function (err, lim) {
             limiter = lim;
             done(err);
           });
           sinon.stub(microtime, 'now').returns(3000);
+          evalScript = sinon.stub(Script.prototype, 'eval');
         });
         afterEach(function () {
           microtime.now.restore();
+          evalScript.restore();
         });
-        it('calls redis.defineCommand with expected arguments when called without id and increment', function (done) {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+        it('calls script.eval with expected arguments when called without id and increment', function (done) {
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           limiter(function (err, data) {
             if (err) {
               return done(err);
             }
             try {
-              expect(options.redis['rateLimiter' + version].args[0][0]).to.eql(
+              expect(evalScript.args[0][1]).to.eql(
                 [2, 'MyNamespace:a', 'MyNamespace:b', 1, 1, 3000, 500 * 1000, 0 * 1000, 5, 25, 1000, 10000, 10000, 10000]);
               expect(data).eql({
                 actionsRemaining: 0,
                 actionsRecorded: 1,
                 wait: 2
-              })
+              });
               return done();
             } catch (err) {
               return done(err);
             }
           });
         });
-        it('calls redis.defineCommand with expected arguments when called with increment', function (done) {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+        it('calls script.eval with expected arguments when called with increment', function (done) {
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           limiter(3, function (err, data) {
             if (err) {
               return done(err);
             }
             try {
-              expect(options.redis['rateLimiter' + version].args[0][0]).to.eql(
+              expect(evalScript.args[0][1]).to.eql(
                 [2, 'MyNamespace:a', 'MyNamespace:b', 1, 3, 3000, 500 * 1000, 0 * 1000, 5, 25, 1000, 10000, 10000, 10000]);
               expect(data).eql({
                 actionsRemaining: 0,
                 actionsRecorded: 1,
                 wait: 2
-              })
+              });
               return done();
             } catch (err) {
               return done(err);
             }
           });
         });
-        it('calls redis.defineCommand with expected arguments when called with id and increment', function (done) {
-          options.redis['rateLimiter' + version].onCall(0).yields(null, [0, 1, 2000]);
+        it('calls script.eval with expected arguments when called with id and increment', function (done) {
+          evalScript.onCall(0).yields(null, [0, 1, 2000]);
           limiter('action', 3, function (err, data) {
             if (err) {
               return done(err);
             }
             try {
-              expect(options.redis['rateLimiter' + version].args[0][0]).to.eql(
+              expect(evalScript.args[0][1]).to.eql(
                 [2, 'MyNamespace:a:action', 'MyNamespace:b:action', 1, 3, 3000, 500 * 1000, 0 * 1000, 5, 25, 1000, 10000, 10000, 10000]);
               expect(data).eql({
                 actionsRemaining: 0,
                 actionsRecorded: 1,
                 wait: 2
-              })
+              });
               return done();
             } catch (err) {
               return done(err);
